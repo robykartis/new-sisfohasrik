c432968eab662c4b442a1a06f3b09251c8b015c4
robykartis <robykarti.s@gmail.com>
2023-01-09 08:48:06 +0700

    RUMAH

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Mungkin Email atau Password Anda Salah.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
